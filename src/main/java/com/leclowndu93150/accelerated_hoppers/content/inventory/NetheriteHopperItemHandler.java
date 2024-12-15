881cbed07b87777c65d389aa9db2d0863877e43a
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-12-15 19:53:15 +0100

    added multiple hoppers

---
+++ src/main/java/com/leclowndu93150/accelerated_hoppers/content/inventory/NetheriteHopperItemHandler.java
@@ -0,0 +1,39 @@
+package com.leclowndu93150.accelerated_hoppers.content.inventory;
+
+import com.leclowndu93150.accelerated_hoppers.Config;
+import com.leclowndu93150.accelerated_hoppers.content.blockentities.NetheriteHopperBlockEntity;
+import com.leclowndu93150.accelerated_hoppers.content.blockentities.WoodenHopperBlockEntity;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.items.wrapper.InvWrapper;
+import org.jetbrains.annotations.NotNull;
+
+public class NetheriteHopperItemHandler extends InvWrapper
+{
+    private final NetheriteHopperBlockEntity hopper;
+
+    public NetheriteHopperItemHandler(NetheriteHopperBlockEntity hopper) {
+        super(hopper);
+        this.hopper = hopper;
+    }
+
+    @Override
+    @NotNull
+    public ItemStack insertItem(int slot, @NotNull ItemStack stack, boolean simulate)
+    {
+        if (simulate) {
+            return super.insertItem(slot, stack, true);
+        } else {
+            boolean wasEmpty = getInv().isEmpty();
+            int originalStackSize = stack.getCount();
+            stack = super.insertItem(slot, stack, false);
+            if (wasEmpty && originalStackSize > stack.getCount())
+            {
+                if (hopper.mayNotTransfer())
+                {
+                    hopper.setTransferCooldown(Config.netheriteHopperTransferCooldown);
+                }
+            }
+            return stack;
+        }
+    }
+}
